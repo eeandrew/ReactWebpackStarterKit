3c90ccccccc9f5140474bea022782a36d1827f73
zhou lin <boyzhoulin@sina.com>
2016-01-27 17:31:34 +0800

    为todo加入进入和退出动画;

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Border from './Border.jsx';
 import AddTodo from './AddTodo.jsx';
 import TodoItem from './TodoItem.jsx';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
 
 export default class Todo extends React.Component {
 	constructor() {
@@ -24,13 +26,31 @@ export default class Todo extends React.Component {
 		this.setState({todos:[...this.state.todos.slice(0,index),todo,...this.state.todos.slice(index+1)]});
 	}
 
+	removeTodo(todo) {
+		let i=0;
+		let index = 0;
+		for(;i<this.state.todos.length;i++) {
+			if(this.state.todos[i].desc === todo.desc){
+				index = i;
+			}
+		}
+		this.setState({todos:[...this.state.todos.slice(0,index),...this.state.todos.slice(index+1)]});
+	}
+
 	todos() {
 		var self = this;
 		return this.state.todos.map(function(todo){
-			return <TodoItem todo={todo} toggleTodo={self.completeTodo.bind(self)}/>;	
+			return <TodoItem todo={todo} toggleTodo={self.completeTodo.bind(self)} removeTodo={self.removeTodo.bind(self)}/>;	
 		})
 	}
 	render() {
-		return <Border>{this.todos()} <AddTodo onAdd={this.addTodo.bind(this)}/></Border>;
+		return (
+			<Border>
+				<ReactCSSTransitionGroup transitionName="todo" transitionEnterTimeout={300} transitionLeaveTimeout={300}>
+					{this.todos()} 
+				</ReactCSSTransitionGroup>
+				<AddTodo onAdd={this.addTodo.bind(this)}/>
+			</Border>
+		);
 	}
 }
--- app/components/TodoItem.jsx
+++ app/components/TodoItem.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import './TodoItem.css';
 
 export default class TodoItem extends React.Component {
 	toggleTodo(){
 		this.props.toggleTodo(Object.assign({},this.props.todo,{completed:!this.props.todo.completed}));
 	}
+	removeTodo() {
+		this.props.removeTodo(this.props.todo);
+	}
 	render() {
-		return <li  style={{textDecoration : this.props.todo.completed?'line-through':''}}><input type="checkbox" onClick={this.toggleTodo.bind(this)} checked={this.props.todo.completed} />{this.props.todo.desc}</li>
+		return (
+			<li className="todoitem" style={{textDecoration : this.props.todo.completed?'line-through':''}}>
+			<input type="checkbox" onClick={this.toggleTodo.bind(this)} checked={this.props.todo.completed} />
+			{this.props.todo.desc}
+			<span className="delete-btn" onClick={this.removeTodo.bind(this)}>delete</span>
+			</li>);
 	}
 }
+
+TodoItem.propTypes = {
+	toggleTodo:React.PropTypes.func.isRequired,
+	todo : React.PropTypes.object.isRequired
+};
