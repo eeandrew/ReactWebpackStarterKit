beb8eac7a6d9e5f723906276b5d6c8d712f9f565
zhoulin <zhoulin@zhoulin900.local>
2016-02-04 17:32:11 +0800

    react左右滑动组件；

--- app/main.jsx
+++ app/main.jsx
@@ -4,6 +4,7 @@ import World from './components/World.jsx';
 import Timer from './components/Timer.jsx';
 import Counter from './components/Counter.jsx';
 import Todo from './components/Todo.jsx';
+import SwipeCtrl from './components/SwipeCtrl.jsx';
 class App extends React.Component {
 	constructor() {
 		super();
@@ -12,10 +13,11 @@ class App extends React.Component {
 	render() {
 		return (
 			<div>
-				<Hello name="Hello"/>
-				<Timer />
-				<Counter />
-				<Todo/>
+				<SwipeCtrl maxSwipe={100} direction='LEFT'>左滑动</SwipeCtrl>
+
+				<SwipeCtrl maxSwipe={200} direction='RIGHT'>右滑动</SwipeCtrl>
+
+				<SwipeCtrl maxSwipe={150} direction='BOTH'>左右</SwipeCtrl>
 			</div>
 			);
 	}
