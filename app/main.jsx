93f6119402c0c89ec7649b82f9d3b0f40df93b46
zhoulin <zhoulin@zhoulin900.local>
2016-02-14 16:35:48 +0800

    自动调整textarea的高度控件

--- app/main.jsx
+++ app/main.jsx
@@ -9,6 +9,7 @@ import SwipeCtrl from './components/SwipeCtrl.jsx';
 import Carousel from './components/carousel/Carousel.jsx';
 import Alert from './components/Alert.jsx';
 import SimpleRemove from './components/SimpleRemove.jsx';
+import AutoSize from './components/AutoSize.jsx'
 
 class App extends React.Component {
 	constructor() {
@@ -35,7 +36,14 @@ function main() {
 	const alertStr = {
 		str : 'hello world'
 	};
-	ReactDom.render(<SimpleRemove />,document.getElementById('app'));
+	const sampleTxt = 'Lorem ipsum dolor sit amet, \
+	consectetur adipiscing elit, sed do eiusmod tempor\
+	 incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\
+	  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo \
+	  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\
+	   cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, \
+	   sunt in culpa qui officia deserunt mollit anim id est laborum';
+	ReactDom.render(<AutoSize>{sampleTxt}</AutoSize>,document.getElementById('app'));
 }
 
 main();
